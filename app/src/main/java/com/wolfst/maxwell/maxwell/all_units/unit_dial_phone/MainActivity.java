8b1ede209bf351564ea254c8c905e97fa8c1266f
humblewolf <humblewolff@gmail.com>
2019-05-06 03:05:21 +0530

    basic app structure added

---
+++ app/src/main/java/com/wolfst/maxwell/maxwell/all_units/unit_dial_phone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wolfst.maxwell.maxwell.all_units.unit_dial_phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.wolfst.maxwell.maxwell.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
